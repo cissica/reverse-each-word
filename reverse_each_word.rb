cb19e88d7a252cace74084ca1572ca7a02c61780
cissica <cecemartinez@hotmail.com>
2020-10-09 10:11:42 -0700

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,12 @@
 def reverse_each_word(string)
+    word = string.split(" ")
+    new_array = []
+    word.each do |word| 
+    new_array << word.reverse
+    end
+    new_array.join (" ")
+end 
+
+def reverse_each_word(string)
     string.split.collect {|word| word.reverse}.join(" ")
 end 
